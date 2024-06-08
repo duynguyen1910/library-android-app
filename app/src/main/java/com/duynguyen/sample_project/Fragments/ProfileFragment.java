f975de9747d33c2a1dd9beb03234d31f765200fd
ngocdai99 <ngocdai99@gmail.com>
2024-06-09 00:21:13 +0700

    create fragment profile

--- app/src/main/java/com/duynguyen/sample_project/Fragments/ProfileFragment.java
+++ app/src/main/java/com/duynguyen/sample_project/Fragments/ProfileFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,11 +14,23 @@ import com.duynguyen.sample_project.R;
 
 public class ProfileFragment extends Fragment {
     View view;
+    LinearLayout layoutLogout;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        Mapping();
+        layoutLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requireActivity().finish();
+            }
+        });
         return view;
+    }
+
+    private void Mapping(){
+        layoutLogout = view.findViewById(R.id.layoutLogout);
     }
 }
